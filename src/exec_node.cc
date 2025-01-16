84219df7a6f4f1538e939dbf3759c00b2261a20f
weiqiangwu <weiqiangwu@tencent.com>
2025-01-16 17:21:28 +0800

    optimize code

--- src/exec_node.cc
+++ src/exec_node.cc
@@ -1,8 +1,21 @@
+/*
+ * @Author: victorika
+ * @Date: 2025-01-15 10:59:27
+ * @Last Modified by: victorika
+ * @Last Modified time: 2025-01-16 16:57:24
+ */
 #include "exec_node.h"
+#include <type.h>
 
 namespace jitfusion {
 
 std::string ExecNode::ToString(const std::string& prefix) { return ToStringImpl(prefix); }
+
+std::string EntryArgumentNode::ToStringImpl(const std::string& prefix) { return prefix + "|--entry_argument\n"; }
+Status EntryArgumentNode::Accept(Visitor* visitor) { return visitor->visit(*this); }
+
+std::string ExecContextNode::ToStringImpl(const std::string& prefix) { return prefix + "|--exec_context\n"; }
+Status ExecContextNode::Accept(Visitor* visitor) { return visitor->visit(*this); }
 
 struct ValueToString {
   template <typename T>
@@ -49,17 +62,29 @@ std::string ConstantListValueNode::ToStringImpl(const std::string& prefix) {
 Status ConstantListValueNode::Accept(Visitor* visitor) { return visitor->visit(*this); }
 
 inline std::string UnaryOPNode::ToStringImpl(const std::string& prefix) {
-  std::string result = prefix + "|--" + UnaryOPTypeToString(op_) + "\n" + child_->ToString(prefix + "|   ");
+  std::string result = prefix + "|--" + TypeHelper::UnaryOPTypeToString(op_) + "\n" + child_->ToString(prefix + "|   ");
   return result;
 }
 
 Status UnaryOPNode::Accept(Visitor* visitor) { return visitor->visit(*this); }
 
 std::string BinaryOPNode::ToStringImpl(const std::string& prefix) {
-  return prefix + "|--" + BinaryOPTypeToString(op_) + "\n" + left_->ToString(prefix + "|   ") +
+  return prefix + "|--" + TypeHelper::BinaryOPTypeToString(op_) + "\n" + left_->ToString(prefix + "|   ") +
          right_->ToString(prefix + "|   ");
 }
 
 Status BinaryOPNode::Accept(Visitor* visitor) { return visitor->visit(*this); }
+
+inline std::string FunctionNode::ToStringImpl(const std::string& prefix) {
+  std::string result = prefix + "|--" + func_name_ + "\n";
+  for (const auto& child : args_) {
+    result += child->ToString(prefix + "|   ");
+  }
+  return result;
+}
+
+void FunctionNode::AppendArgs(std::unique_ptr<ExecNode>&& arg) { args_.emplace_back(std::move(arg)); }
+
+Status FunctionNode::Accept(Visitor* visitor) { return visitor->visit(*this); }
 
 }  // namespace jitfusion
