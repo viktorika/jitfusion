0572ba950f57b60d66d84b49c72da145af48203a
weiqiangwu <weiqiangwu@tencent.com>
2025-05-12 14:20:36 +0800

    optimize ListLog and ListLog2 with xsimd

--- src/function/list_function_init.cc
+++ src/function/list_function_init.cc
@@ -373,9 +373,23 @@ ResultType ListLog(ListType a, void *exec_context) {
   result.data = reinterpret_cast<typename ResultType::CElementType *>(
       exec_ctx->arena.Allocate((a.len) * sizeof(typename ResultType::CElementType)));
   result.len = a.len;
+#ifdef HAS_XSIMD
+  using batch_type = xs::batch<typename ResultType::CElementType, xs::default_arch>;
+  constexpr std::size_t batch_size = batch_type::size;
+  auto vec_size = a.len - (a.len % batch_size);
+  for (std::size_t i = 0; i < vec_size; i += batch_size) {
+    auto a_vec = batch_type::load_unaligned(a.data + i);
+    auto log_vec = xs::log(a_vec);
+    log_vec.store_unaligned(result.data + i);
+  }
+  for (std::size_t i = vec_size; i < a.len; ++i) {
+    result.data[i] = std::log(a.data[i]);
+  }
+#else
   for (uint32_t i = 0; i < a.len; i++) {
     result.data[i] = std::log(a.data[i]);
   }
+#endif
   return result;
 }
 
@@ -386,9 +400,23 @@ ResultType ListLog2(ListType a, void *exec_context) {
   result.data = reinterpret_cast<typename ResultType::CElementType *>(
       exec_ctx->arena.Allocate((a.len) * sizeof(typename ResultType::CElementType)));
   result.len = a.len;
+#ifdef HAS_XSIMD
+  using batch_type = xs::batch<typename ResultType::CElementType, xs::default_arch>;
+  constexpr std::size_t batch_size = batch_type::size;
+  auto vec_size = a.len - (a.len % batch_size);
+  for (std::size_t i = 0; i < vec_size; i += batch_size) {
+    auto a_vec = batch_type::load_unaligned(a.data + i);
+    auto log_vec = xs::log2(a_vec);
+    log_vec.store_unaligned(result.data + i);
+  }
+  for (std::size_t i = vec_size; i < a.len; ++i) {
+    result.data[i] = std::log2(a.data[i]);
+  }
+#else
   for (uint32_t i = 0; i < a.len; i++) {
     result.data[i] = std::log2(a.data[i]);
   }
+#endif
   return result;
 }
 
