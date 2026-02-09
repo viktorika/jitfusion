335ca5bf3a842a82de81972011d5011a38b389ff
weiqiangwu <weiqiangwu@tencent.com>
2026-02-09 14:59:58 +0800

    Disassemble list_function_init.cc

--- src/function/function_init.h
+++ src/function/function_init.h
@@ -2,7 +2,7 @@
  * @Author: victorika
  * @Date: 2025-01-23 10:37:20
  * @Last Modified by: victorika
- * @Last Modified time: 2026-02-09 14:31:05
+ * @Last Modified time: 2026-02-09 14:48:41
  */
 #include "function_registry.h"
 #include "status.h"
@@ -14,5 +14,6 @@ Status InitStringInternalFunc(FunctionRegistry *reg);
 Status InitListInternalFunc(FunctionRegistry *reg);
 
 Status InitListArithmeticFunc(FunctionRegistry *reg);
+Status InitListComparisonFunc(FunctionRegistry *reg);
 
 }  // namespace jitfusion
