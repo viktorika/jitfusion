b5e1c1cc33bc325aca38e63da5c59bcf0349a95a
weiqiangwu <weiqiangwu@tencent.com>
2026-03-04 11:32:53 +0800

    add NumericTypeConvert return check

--- src/codegen/if_codegen.cc
+++ src/codegen/if_codegen.cc
@@ -50,7 +50,8 @@ Status CodeGen::Visit(IfNode &if_node) {
   ctx_.builder.SetInsertPoint(then_block);
   llvm::Value *then_value{};
   JF_RETURN_NOT_OK(GetValue(if_node.GetArgs()[1].get(), &then_value));
-  NumericTypeConvert(ctx_, if_node.GetArgs()[1]->GetReturnType(), if_node.GetReturnType(), &then_value);
+  JF_RETURN_NOT_OK(
+      NumericTypeConvert(ctx_, if_node.GetArgs()[1]->GetReturnType(), if_node.GetReturnType(), &then_value));
   ctx_.builder.CreateBr(merge_block);
 
   // 创建 else 块
@@ -59,7 +60,8 @@ Status CodeGen::Visit(IfNode &if_node) {
   ctx_.builder.SetInsertPoint(else_block);
   llvm::Value *else_value{};
   JF_RETURN_NOT_OK(GetValue(if_node.GetArgs()[2].get(), &else_value));
-  NumericTypeConvert(ctx_, if_node.GetArgs()[2]->GetReturnType(), if_node.GetReturnType(), &else_value);
+  JF_RETURN_NOT_OK(
+      NumericTypeConvert(ctx_, if_node.GetArgs()[2]->GetReturnType(), if_node.GetReturnType(), &else_value));
   ctx_.builder.CreateBr(merge_block);
 
   // 创建 merge 块
